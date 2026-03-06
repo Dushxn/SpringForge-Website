771cb8c186ae0d88c86333837e4db6e21c7f5e11
Tharindu Dushan <tdmahindarathne@gmail.com>
2026-03-06 17:19:55 +0530

    fix/added health endpoint to test

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { PluginModule } from './plugin/plugin.module';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { FeedbackModule } from './feedback/feedback.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { FeedbackModule } from './feedback/feedback.module';
     PluginModule,
     AdminModule,
     FeedbackModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
